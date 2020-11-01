820e372b5b675d1ab2546a508e8bf3e13b7f1be7
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-11-01 15:27:28 +0530

    Generator updated

--- Re_Generator.py
+++ Re_Generator.py
@@ -1,6 +1,9 @@
 # Generator are iterators and functions in python very good for enforcing sequence
 
 def customContextManager():
+    '''
+    Function to enforce that a connection is established before a table is accessed
+    '''
     print('Connection Established')
     yield
     print('Connection Closed')
