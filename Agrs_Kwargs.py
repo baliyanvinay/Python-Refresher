1e66e11a1fe064ab985dc722bd2744fb1f6267c9
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-11-17 22:22:09 +0530

    Kwargs to create dynamic class object

--- Agrs_Kwargs.py
+++ Agrs_Kwargs.py
@@ -1,5 +1,6 @@
 # Agrs and Kwargs for arbitary number of agruements
 
+# Agrs takes n number of non-keyword agruements and several operations can be performed with it then
 def an_agrs_func(*agrs):
     val=0
     for a in agrs:
@@ -7,3 +8,13 @@ def an_agrs_func(*agrs):
     return val
 
 print(an_agrs_func(3,4,5))
+
+# kwargs returns a dict which can be used to create some objects
+class Developer:
+    def __init__(self, **kwargs):
+        for kwarg in kwargs:
+            self.kwarg=kwargs[kwarg] # assigning key as value
+
+new_developer=Developer(name='Vinay',age=26, role='Backend Engineer')
+print(new_developer)
+
