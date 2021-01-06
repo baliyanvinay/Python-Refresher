ed759b8879f7817f0d9e9bbb781340c51953ca80
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-01-06 12:18:38 +0530

    Output also added

--- List vs Tuples.py
+++ List vs Tuples.py
@@ -13,3 +13,9 @@ res = timeit.timeit('for i in (1,2,3,4,5): pass')
 print('Tuple Indexing: ', res)
 res = timeit.timeit('for i in [1,2,3,4,5]: pass')
 print('List Indexing: ', res)
+
+# Output
+# Tuple construction:  0.036594656
+# List construction:  0.167624964
+# Tuple Indexing:  0.33929154899999997
+# List Indexing:  0.350877805
