ed9752ed459d840079289abb557a2904e5a63bde
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-01-06 10:31:04 +0530

    Mutable and immutable code complete

--- Mutable and Immutable.py
+++ Mutable and Immutable.py
@@ -4,26 +4,48 @@ def print_object(var):
     print('ID of object: ', id(var))
     print('Type of object : ', type(var))
     print('Value of object : ', var)
-    print('\n')
+
 
 # Mutable data types in Python
+# Lists
+var = [17, 10]
+print_object(var)
+var += [17]
+print_object(var)
+print('\n')
 
+# Dictionary
+var = {'first_name': 'Vinay', }
+print_object(var)
+var['last_name'] = 'Baliyan'
+print_object(var)
+print('\n')
+
+# Set
+var = {1, 2}
+print_object(var)
+var.update({2, 3})  # to add values in current set
+print_object(var)
+print('\n')
 
 # Immutable objects/data types in Python
-var = 17  # Do note that the object here is 17 and not var, var is the variable holding object 17
+# Numeric values
+var = 17.0  # Do note that the object here is 17 and not var, var is the variable holding object 17
 print_object(var)
-
-var = 89
+var += 10
 print_object(var)
+print('\n')
 
-var = 'name'
+# Strings
+var = 'Vinay'
 print_object(var)
-
-var = 78.0
+var += ' Baliyan'
 print_object(var)
+print('\n')
 
-var = ('Grey', 17)  # a tuple
+# Tupple
+var = ('Grey',)
 print_object(var)
-
-var = ('Grey', 7)  # made changes in tuple
+var += (17,)  # made changes in tuple
 print_object(var)  # result will be a different ID
+print('\n')
