453c9bbbf5a1479d235443f9c3a5465e2c22be5b
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-01-11 17:56:40 +0530

    Exception handling file added

---
+++ Exception Handling.py
@@ -0,0 +1,15 @@
+# Example of index out of range exception or IndexError
+
+array_list = [1, 2, 3]  # with indexes 0,1,2
+try:
+    print('In try block: accessing index 3')
+    array_list[3] = 12
+except IndexError:
+    print('In except block: Error Occured')
+    array_list.append(12)
+else:
+    print('In else block: No error')
+    array_list.reverse()
+finally:
+    print('In finally block: Printing list')
+    print(array_list)
