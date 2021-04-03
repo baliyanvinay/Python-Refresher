b80aa4c3d2a34b814e96a0f6e05da846754343eb
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-04-03 23:59:28 +0530

    Another singleton example added

--- Design Patterns/Singleton.py
+++ Design Patterns/Singleton.py
@@ -11,6 +11,22 @@ def Database_Connect():
         _instance = _Database_Connect()
     return _instance
 
+
+# Overiding __new__ of class || There is only one govt in a country 
+class Government:
+    _instance = None
+    def __new__(cls, country_name):
+        if not cls._instance: # if cls._instance is None
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self, country_name):
+        self.country_name = country_name
+    
+    def __str__(self):
+        return f"Government of {self.country_name}"
+
+
 if __name__ == '__main__':
     db_con = Database_Connect()
     print(db_con)
@@ -19,3 +35,9 @@ if __name__ == '__main__':
     print(db_con_again)
 
     print(db_con is db_con_again)
+
+    # Government Singleton
+    indian_govt = Government('India')
+    bharat_govt = Government('India')
+    print(indian_govt, ' ', bharat_govt)
+    print(indian_govt is bharat_govt)
